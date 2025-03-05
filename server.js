232788d3bb1336a8e7b3335fe6b611e8b38abb2b
Cesar Preza <cpreza93@yahoo.com>
2025-03-04 20:09:21 -0800

    adds authorization and index route

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+// TOKEN eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJuYW1lIjoidGVzdCIsIl9pZCI6IjY3YzdjNjBmZjEzZjhiYWJkN2Q2MWM5MiJ9LCJpYXQiOjE3NDExNDU2MTV9.Fv95-sY4oYtwXODhyvp1PIRpCNQ3NzNBWuaK8U9y9AA
+
 // Import routers
 const authRouter = require('./controllers/auth');
 const testJwtRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
+const hootsRouter = require('./controllers/hoots');
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
@@ -28,6 +31,7 @@ app.use(logger('dev'));
 app.use('/auth', authRouter);
 app.use('/test-jwt', testJwtRouter);
 app.use('/users', usersRouter);
+app.use('/hoots', hootsRouter);
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
